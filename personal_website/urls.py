9719c2ce8bc687e991c554612c1fca160863c96a
Dean <lynchdean@gmail.com>
2020-02-27 22:56:56 +0000

    Add site warning and change project card layout

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls'), name='project_index'),
+    path('', include('projects.urls'), name='project_index'),
 ]
